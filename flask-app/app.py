70035bd067c64d72c464d6e210a2f6000bd23a0f
Ubuntu <ubuntu@ip-172-31-45-228.ap-south-1.compute.internal>
2024-12-03 18:20:57 +0000

    Trigger workflow

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Hello, World!"
+    return "Let us Deploy a Python Flask Application on EKS using GitHub Actions"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
